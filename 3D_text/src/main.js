7b916004b2d54c0dff78beeadcbd591af7e0df61
everydayHo <jungwoopage@gmail.com>
2024-05-12 12:34:41 +0900

    create 3D text

---
+++ 3D_text/src/main.js
@@ -0,0 +1,149 @@
+import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import GUI from 'lil-gui';
+
+window.addEventListener('load', function () {
+	init();
+});
+
+async function init() {
+	const gui = new GUI();
+	const renderer = new THREE.WebGLRenderer({
+		antialias: true,
+	});
+	renderer.shadowMap.enabled = true;
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	document.body.appendChild(renderer.domElement);
+
+	const scene = new THREE.Scene();
+
+	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 500);
+
+	camera.position.set(0, 1, 5);
+
+	/** controls */
+	new OrbitControls(camera, renderer.domElement);
+
+	/** 폰트 */
+
+	const fontLoader = new FontLoader();
+
+	const font = await fontLoader.loadAsync('./assets/fonts/The Jamsil 3 Regular_Regular.json');
+
+	const textGeometry = new TextGeometry('THREE.js Interactive Motion ', {
+		font,
+		size: 0.5,
+		depth: 0.1,
+		bevelEnabled: true,
+		bevelSegments: 5,
+		bevelSize: 0.02,
+		bevelThickness: 0.02,
+	});
+	const textMaterial = new THREE.MeshPhongMaterial();
+	const text = new THREE.Mesh(textGeometry, textMaterial);
+	// textGeometry.computeBoundingBox();
+	// textGeometry.translate(
+	// 	-(textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x) * 0.5,
+	// 	-(textGeometry.boundingBox.max.y - textGeometry.boundingBox.min.y) * 0.5,
+	// 	-(textGeometry.boundingBox.max.z - textGeometry.boundingBox.min.z) * 0.5
+	// );
+	text.castShadow = true;
+	textGeometry.center();
+	/** Texture */
+	const textureLoader = new THREE.TextureLoader().setPath('/assets/textures/');
+	const textTexture = textureLoader.load('holographic.jpeg');
+	textMaterial.map = textTexture;
+
+	scene.add(text);
+	/** Plane */
+
+	const planGeometry = new THREE.PlaneGeometry(2000, 2000);
+	const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x000000 });
+	const plane = new THREE.Mesh(planGeometry, planeMaterial);
+
+	plane.position.z = -10;
+	plane.receiveShadow = true;
+	scene.add(plane);
+	/** AmbientLight */
+	const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+	scene.add(ambientLight);
+
+	const spotLight = new THREE.SpotLight(0xffffff, 8, 30, Math.PI * 0.15, 0.2, 0.5);
+	spotLight.position.set(0, 0, 3);
+	spotLight.target.position.set(0, 0, -3);
+	spotLight.castShadow = true;
+	spotLight.shadow.mapSize.width = 1024;
+	spotLight.shadow.mapSize.height = 1024;
+	spotLight.shadow.radius = 10;
+	const spotLightTexture = textureLoader.load('gradient.jpg');
+	spotLightTexture.encoding = THREE.sRGBEncoding;
+	spotLight.map = spotLightTexture;
+	scene.add(spotLight, spotLight.target);
+
+	window,
+		addEventListener('mousemove', (e) => {
+			const x = (e.clientX / window.innerWidth - 0.5) * 5;
+			const y = -(e.clientY / window.innerHeight - 0.5) * 5;
+			spotLight.target.position.set(x, y, -3);
+		});
+
+	const spotLightFolder = gui.addFolder('spotLight');
+	spotLightFolder
+		.add(spotLight, 'angle')
+		.min(0)
+		.max(Math.PI / 2)
+		.step(0.01);
+
+	spotLightFolder.add(spotLight.position, 'z').min(1).max(10).step(0.01).name('position.z');
+	spotLightFolder.add(spotLight, 'distance').min(1).max(30).step(0.01).name('distance');
+	spotLightFolder.add(spotLight, 'decay').min(0).max(10).step(0.01).name('decay');
+	spotLightFolder.add(spotLight, 'penumbra').min(0).max(1).step(0.01).name('penumbra');
+	spotLightFolder.add(spotLight.shadow, 'radius').min(1).max(20).step(0.01).name('shadow.radius');
+	// pointLight
+
+	// const pointLight = new THREE.PointLight(0xffffff, 0.5);
+	// const pointlightHelper = new THREE.PointLightHelper(pointLight, 0.5);
+	// pointLight.position.set(3, 0, 2);
+	// scene.add(pointLight);
+
+	// gui.add(pointLight.position, 'x').min(-3).max(3).step(0.1);
+
+	/** Effect */
+	const composer = new EffectComposer(renderer);
+	const renderPass = new RenderPass(scene, camera);
+	composer.addPass(renderPass);
+
+	const unrealBloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 0.42, 1, 0);
+	composer.addPass(unrealBloomPass);
+
+	const unrealBloomPassFolder = gui.addFolder('UnrealBloomPass');
+	unrealBloomPassFolder.add(unrealBloomPass, 'strength').min(0).max(3).step(0.01);
+	unrealBloomPassFolder.add(unrealBloomPass, 'radius').min(0).max(1).step(0.01);
+	unrealBloomPassFolder.add(unrealBloomPass, 'threshold').min(0).max(1).step(0.01);
+	render();
+
+	function render() {
+		composer.render();
+		// renderer.render(scene, camera);
+
+		requestAnimationFrame(render);
+	}
+
+	function handleResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(window.innerWidth, window.innerHeight);
+
+		renderer.render(scene, camera);
+	}
+
+	window.addEventListener('resize', handleResize);
+}
